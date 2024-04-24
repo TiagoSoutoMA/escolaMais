e4bae028f15eb922b382eb7102c31febc981526c
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2024-04-23 23:13:05 -0300

    Create: Componentes criados, faltando apenas regularizar o css modules para parar de dar conflito

---
+++ project-school/src/App.jsx
@@ -0,0 +1,20 @@
+import Home from './components/home/Home.jsx';
+import Login from './components/login/Login.jsx';
+import Register from './components/register/Register.jsx';
+
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element = {<Home />} />     
+        <Route path = '/login' element = {<Login />} /> 
+        <Route path = '/register' element = {<Register />} />   
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ project-school/src/components/login/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import LoginStyle from './Login.module.css'
+import Email from '../../assets/img/icons8-gmail-novo-48.png'
+
+const Login = () => {
+    return(
+        <>
+            <div className='corpo'>
+                <div className="login-container">
+                    <h1 className='entrar'>Entrar</h1>
+                    <form action="#" method="POST">
+                        <div className="input-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="password">Senha:</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <div className="remember-me">
+                            <input type="checkbox" id="remember" name="remember" />
+                            <label htmlFor="remember">Lembrar a senha</label>
+                            <a href="/" className="forgot-passwordd">Esqueci a senha</a>
+                        </div>
+
+                        <div className="button-group">
+                            <button type="button" className="google-login">Entrar com Gmail<img  src={Email} alt=""/> </button>
+                            <button type="submit">Entrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
