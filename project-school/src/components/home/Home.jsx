ebc0805428c941eaa5a379b8e5b439585dd929f2
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2024-05-12 11:32:28 -0300

    Alteração na logo do projeto

--- project-school/src/components/home/Home.jsx
+++ project-school/src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Logo from '../../assets/img/Logo2.png';
+import Logo from '../../assets/img/logoEscola+.png';
 import HomeStyle from './Home.module.css';
 import { Link } from 'react-router-dom';
 
