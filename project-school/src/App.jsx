a39fa07a4a5564a2d7c32f701ca098c02743211b
henrique <henriquekennedy.10@gmail.com>
2024-05-13 17:56:44 -0300

    Início da construção da página de usuários

--- project-school/src/App.jsx
+++ project-school/src/App.jsx
@@ -1,5 +1,5 @@
 import Home from './components/home/Home.jsx';
-
+import Users from './components/users/Users.jsx'
 import Register from './components/register/Register.jsx';
 
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
@@ -12,6 +12,7 @@ function App() {
         <Route path = '/' element = {<Home />} />     
        
         <Route path = '/register' element = {<Register />} />   
+        <Route path = '/users' element = {<Users />} />   
       </Routes>
     </BrowserRouter>
   )
